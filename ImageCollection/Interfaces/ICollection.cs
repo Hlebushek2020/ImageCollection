62c595fceda6899fea61f20c2adfcd43849e4f7c
Сергей <63193749+Hlebushek2020@users.noreply.github.com>
2021-05-18 17:51:21 +0300

    Update 18-05-2021
    Add Interface ICollection<T>

---
+++ ImageCollection/Interfaces/ICollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImageCollection.Interfaces
+{
+    interface ICollection<T>
+    {
+        Guid Id { get; }
+        IEnumerable<string> ActualItems { get; }
+        IEnumerable<string> IrrelevantItems { get; }
+        string Description { get; set; }
+        bool IsChanged { get; set; }
+
+        void AddIgnorRules(string item, bool inCurrentFolder, Guid? parent);
+        void Add(string item, bool inCurrentFolder, Guid? parent);
+        void RemoveIgnorRules(string item);
+        void Remove(string item);
+        void Rename(string oldName, string newName);
+        T GetMeta(string item);
+        void ClearIrrelevantItems();
+    }
+}
